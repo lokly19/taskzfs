9c18d3c7cd70a74a5c2d5e022a5ee70ab545ae47
lokly19 <152083133+lokly19@users.noreply.github.com>
2023-12-11 00:51:17 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,8 @@ zfs set compression=lz4 otus2
 zfs set compression=gzip-9 otus3
 zfs set compression=zle otus4
 
+wget -O archive.tar.gz --no-check-certificate 'https://drive.usercontent.google.com/download?id=1MvrcEp-WgAQe57aDEzxSRalPAwbNN1Bb&export=download'
+tar -xzvf archive.tar.gz
+zpool import -d zpoolexport/
+zpool import -d zpoolexport/ otus
+zpool status
